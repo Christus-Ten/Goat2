6cd88f2e6bc54afc74db2fc6af181ed22beede07
Christus-Ten <voldigoa82@gmail.com>
2026-02-21 12:44:36 +0000

    Update spy.js

--- scripts/cmds/spy.js
+++ scripts/cmds/spy.js
@@ -1,4 +1,4 @@
-const fonts = require('../../func/fonts.js');
+const fonts = require('../../func/font.js');
 const axios = require('axios');
 
 module.exports = {
@@ -267,4 +267,4 @@ function getUserRank(exp) {
 	if (exp < 50000) return "💎 Expert";
 	if (exp < 100000) return "👑 Master";
 	return "🌟 Legend";
-  }
+        }
